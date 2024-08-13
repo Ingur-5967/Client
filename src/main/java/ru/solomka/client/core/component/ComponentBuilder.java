a19513f1e5429a606c98986322126f91d2ff0fe2
Solomka <76917656+Ingur-5967@users.noreply.github.com>
2024-08-13 22:57:16 +0300

    Update ComponentBuilder.java

--- src/main/java/ru/solomka/client/core/component/ComponentBuilder.java
+++ src/main/java/ru/solomka/client/core/component/ComponentBuilder.java
@@ -39,37 +39,9 @@ public class ComponentBuilder<T extends Node> {
     }
 
     public ImageWrapper wrapper() {
-
         if(!(source instanceof ImageView))
             throw new IllegalArgumentException("Component must have a image");
-
-//        ImageView logo = new ImageView(new File("D:\\Profile-test.png").getPath());
-//
-//        logo.setFitHeight(30);
-//        logo.setFitWidth(30);
-//
-//
-//        // set a clip to apply rounded border to the original image.
-//        Rectangle clip = new Rectangle(
-//                logo.getFitWidth(), logo.getFitHeight()
-//        );
-//        clip.setArcWidth(30);
-//        clip.setArcHeight(30);
-//        logo.setClip(clip);
-//
-//        // snapshot the rounded image.
-//        SnapshotParameters parameters = new SnapshotParameters();
-//        parameters.setFill(Color.TRANSPARENT);
-//        WritableImage image = logo.snapshot(parameters, null);
-//
-//        // remove the rounding clip so that our effect can show through.
-//        logo.setClip(null);
-//
-//        // apply a shadow effect.
-//        logo.setEffect(new DropShadow(5, Color.BLACK));
-//
-//        // store the rounded image in the imageView.
-//        logo.setImage(image);
+        
         return new ImageWrapper(((ImageView) source).getImage());
     }
 
